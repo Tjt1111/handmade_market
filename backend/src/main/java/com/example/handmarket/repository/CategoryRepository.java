b43f48658cff8ded73bc3d9c16b79af26b22a098
Wang Dengke <2914081643@qq.com>
2026-04-17 13:42:39 +0800

    111

--- backend/src/main/java/com/example/handmarket/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.handmarket.repository;
-
-import com.example.handmarket.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
